1fb16891aa87c2ff5bf3473ea90ebf7153ea7101
jrluiscarvalho <jrluiscarvalho@gmail.com>
2016-12-19 00:47:09 -0200

    implantando validações e mvc

--- app/models/NoticiasDAO.js
+++ app/models/NoticiasDAO.js
@@ -1,7 +1,7 @@
 function NoticiasDAO(connection){
     this._connection = connection;
 }
-    
+
     NoticiasDAO.prototype.getNoticias = function(callback) {
         this._connection.query('select * from noticias', callback);
     }
@@ -11,6 +11,7 @@ function NoticiasDAO(connection){
     }
 
     NoticiasDAO.prototype.salvarNoticias = function(noticia, callback){
+
         this._connection.query('insert into noticias set ?', noticia, callback);
     }
 
--- config/server.js
+++ config/server.js
@@ -2,16 +2,19 @@ var express = require('express');
 var consign = require('consign');
 var app = express();
 var bodyParser = require('body-parser');
+var expressValidator = require('express-validator')
 
 app.set('view engine', 'ejs');
 app.set('views', './app/views');
 
 app.use(bodyParser.urlencoded({extended:true}));
+app.use(expressValidator());
 
 consign()
 	.include('app/routes')
 	.then('config/dbConnection.js')
-    .then('app/models')
+             .then('app/models')
+             .then('app/controllers')
 	.into(app);
 
 
